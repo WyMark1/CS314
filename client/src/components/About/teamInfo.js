5d28eaba450b7dbea54abd7078f1340f3c6213cc
Austin Harris <hca1197@topeka.cs.colostate.edu>
2024-01-29 21:58:22 -0700

    add austin's image to teamInfo.js

--- client/src/components/About/teamInfo.js
+++ client/src/components/About/teamInfo.js
@@ -1,5 +1,6 @@
 import memberPic from "@static/images/Placeholder.jpg";
 import wyattPic from "@static/images/WyattPic.jpg";
+import austinPic from "@static/images/AustinPic.jpg";
 import teamPic from "@static/images/PlaceholderTeam.jpg";
 
 export const teamData =
@@ -15,7 +16,7 @@ export const memberData = [
         name: "Austin Harris",
         bio: "My name is Austin Harris I was born and raised in Michigan but have lived in Denver for the last 4 years, I am a third year online CS student at Colorado State University. While attending school, I am also a full time employee at Ford Motor Company. My current goals are to continue working through my courses and to use this course in particular to better prepare myself for future internships or employment. Long-term I hope to finish my education and eventually move to a career in the Data Science industry.",
         homeTown: "Byron, Michigan",
-        imagePath: memberPic
+        imagePath: austinPic
     },
     {
         name: "Wyatt Markham",
