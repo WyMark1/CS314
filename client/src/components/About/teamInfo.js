513c0ae4d91d493cbc1d97262a894a02bfe02dc7
brownbpcolostate <brownbp@colostate.edu>
2024-01-31 15:23:14 -0700

    Added to Blakes Bio

--- client/src/components/About/teamInfo.js
+++ client/src/components/About/teamInfo.js
@@ -31,9 +31,9 @@ export const memberData = [
         imagePath: memberPic
     },
     {
-        name: "Team Member 4",
-        bio: "Bio Here",
-        homeTown: "Hometown",
+        name: "Blake Brown",
+        bio: "Hello there! My name is Blake Brown and I am a senior at Colorado State University. I am passionate about programming and everything related to computers. I have been working in a computer repair store for seven years now, and I have loved every second of it. My long-term goal is to become a software engineer for a tech company.",
+        homeTown: "Castle Rock, Colorado",
         imagePath: memberPic
     },
     {
