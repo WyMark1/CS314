f2abc41d91fa46e49a829462a0b592b87f212446
Josh1302 <josh1302@colostate.edu>
2024-02-13 14:03:41 -0700

    added haversine.java to com.tco.misc

---
+++ server/src/main/java/com/tco/misc/haversine.java
@@ -0,0 +1,7 @@
+package com.tco.misc;
+
+public class haversine implements GreatCircleDistance {
+     public Long between(GeographicCoordinate geoCord1, GeographicCoordinate geoCord2, double num){
+        return 0L;
+    }
+}
