aafb6bcd8b0cb3bd8788ad28ce66e24215798db2
Josh1302 <josh1302@colostate.edu>
2024-02-13 12:29:17 -0700

    Added Vincenty.java to com.tco.misc

---
+++ server/src/main/java/com/tco/misc/vincenty.java
@@ -0,0 +1,9 @@
+package main.java.com.tco.misc;
+
+public class vincenty implements GreatCircleDistance {
+
+    public Long between(GeographicCoordinate geoCord1, GeographicCoordinate geoCord2, double num){
+        return 0L;
+    }
+    
+}
