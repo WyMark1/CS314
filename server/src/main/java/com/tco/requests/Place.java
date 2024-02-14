760d20af2c0e829bf633dca0dc3e271eb2e78e0a
Josh1302 <josh1302@colostate.edu>
2024-02-14 16:32:00 -0700

    fixed the place constructor

--- server/src/main/java/com/tco/requests/Place.java
+++ server/src/main/java/com/tco/requests/Place.java
@@ -3,13 +3,15 @@ import com.tco.misc.GeographicCoordinate;
 import java.util.LinkedHashMap;
 
 class Place extends LinkedHashMap<String, String> implements GeographicCoordinate {
+    public double lat;
+    public double lon;
 
     Place (String lat, String lon) {
 
     }
     Place (double lat, double lon){
-        this.lat = latRadians();
-        this.lon = lonRadians();
+        this.lat = lat;
+        this.lon = lon;
     }
 
     public Double latRadians() { return 0.0; }
