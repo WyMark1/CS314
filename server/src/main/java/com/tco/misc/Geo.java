abb44f7b25d29a947770b5e7a7f95927094fd143
Austin Harris <hca1197@montpelier.cs.colostate.edu>
2024-02-21 20:24:50 -0700

    Removed unused files

--- server/src/main/java/com/tco/misc/Geo.java
+++
@@ -1,9 +0,0 @@
-package com.tco.misc;
-
-public class Geo implements GeographicCoordinate{
-    Geo (double lat, double lon){
-        
-    }
-    public Double latRadians() { return 0.0; }
-    public Double lonRadians() { return 0.0; }
-}
