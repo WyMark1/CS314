455491d408406b4bacd55938278c7a2445a6a032
Josh1302 <josh1302@colostate.edu>
2024-02-21 21:12:08 -0700

    Fixed vincenty test- Passes all test cases

--- server/src/test/java/com/tco/misc/TestVincenty.java
+++ server/src/test/java/com/tco/misc/TestVincenty.java
@@ -14,19 +14,19 @@ public class TestVincenty {
         Place location1 = new Place(68.33, 10.68);
         Place location2 = new Place(12.33, 120.22);
         Double radius = 3456789.10;
-        Long result = 546179L;
+        Long result = 5160721L;
         vincenty Vincenty = new vincenty();
-        //assertEquals(result, Vincenty.between(location1, location2, radius));
+        assertEquals(result, Vincenty.between(location1, location2, radius));
     }
 
     @Test
     @DisplayName("Josh1302: Testing vincenty with actual coordinates.")
     public void TestVincentyReal() {
-        Place location1 = new Place(40.17743, -105.09784);
-        Place location2 = new Place(6.69234, -1.60985);
-        Double radius = 637234.82;
-        Long result = 546179L;
+        Place location1 = new Place(-29.31689, 27.48608);
+        Place location2 = new Place(48.20335, 16.37262);
+        Double radius = 10241024.10;
+        Long result = 13970101L;
         vincenty Vincenty = new vincenty();
-        //assertEquals(result, Vincenty.between(location1, location2, radius));
+        assertEquals(result, Vincenty.between(location1, location2, radius));
     }
 }
