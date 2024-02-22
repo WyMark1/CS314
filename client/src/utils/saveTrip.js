e15d8c620227026955ad513d5293dbd6af2d5d2e
brownbpcolostate <brownbp@colostate.edu>
2024-02-21 21:15:24 -0700

    Fixed

--- client/src/utils/saveTrip.js
+++ client/src/utils/saveTrip.js
@@ -1,18 +1,27 @@
 export function SaveTrip(tripName, jsonText) {
-    // Parse the JSON text to an object
-    const jsonObj = JSON.parse(jsonText);
+    // Function to convert JSON text to KML format
+    function jsonToKML(jsonText) {
+        const jsonObj = JSON.parse(jsonText);
+        let kmlText = '<?xml version="1.0" encoding="UTF-8"?>\n<kml xmlns="http://www.opengis.net/kml/2.2">\n<Document>\n';
+        
+        jsonObj.places.forEach(place => {
+            kmlText += '    <Placemark>\n';
+            kmlText += `        <name>${place.defaultDisplayName || place.name || 'Unnamed Place'}</name>\n`;
+            if (place.description) {
+                kmlText += `        <description>${place.description}</description>\n`;
+            }
+            kmlText += '        <Point>\n';
+            kmlText += `            <coordinates>${place.longitude},${place.latitude}</coordinates>\n`;
+            kmlText += '        </Point>\n';
+            kmlText += '    </Placemark>\n';
+        });
 
-    // Convert the JSON object to KML format
-    let kmlText = '<?xml version="1.0" encoding="UTF-8"?>\n<kml xmlns="http://www.opengis.net/kml/2.2">\n<Document>\n';
-    jsonObj.places.forEach(place => {
-        kmlText += '    <Placemark>\n';
-        kmlText += `        <name>${place.defaultDisplayName || 'Unnamed Place'}</name>\n`;
-        kmlText += '        <Point>\n';
-        kmlText += `            <coordinates>${place.longitude},${place.latitude},0</coordinates>\n`;
-        kmlText += '        </Point>\n';
-        kmlText += '    </Placemark>\n';
-    });
-    kmlText += '</Document>\n</kml>';
+        kmlText += '</Document>\n</kml>';
+        return kmlText;
+    }
+
+    // Convert the JSON text to KML
+    const kmlText = jsonToKML(jsonText);
 
     // Create a Blob from the KML text
     const file = new Blob([kmlText], { type: "application/vnd.google-earth.kml+xml" });
@@ -22,7 +31,7 @@ export function SaveTrip(tripName, jsonText) {
     link.download = tripName + ".kml";
     document.body.appendChild(link);
     link.click();
-
+    
     setTimeout(function() {
         document.body.removeChild(link);
         window.URL.revokeObjectURL(url);
