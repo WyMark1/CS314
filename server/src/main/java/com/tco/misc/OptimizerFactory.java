f4a6600d3f80db6e65ffa283d4729d6249f46989
Joshua Underwood <119467345+Josh1302@users.noreply.github.com>
2024-02-28 20:40:23 -0700

    Update OptimizerFactory.java

--- server/src/main/java/com/tco/misc/OptimizerFactory.java
+++ server/src/main/java/com/tco/misc/OptimizerFactory.java
@@ -3,7 +3,7 @@ package com.tco.misc;
 public class OptimizerFactory {
     public OptimizerFactory(){}
 
-    public GreatCircleDistance get(int N, Double response){
+    public TourOptimizer get(int N, Double response){
         return null;
     }
 }
