44a41473f2ae77b2d2cf5d5395b405e96fe1be9d
Josh1302 <josh1302@colostate.edu>
2024-02-28 20:05:03 -0700

    Defined the optimizer factory class

---
+++ server/src/main/java/com/tco/misc/OptimizerFactory.java
@@ -0,0 +1,9 @@
+package com.tco.misc;
+
+public class OptimizerFactory {
+    public OptimizerFactory(){}
+
+    public GreatCircleDistance get(int N, Double response){
+        return null;
+    }
+}
