8c2d0637bec16118fc8e0e4216e509eb2e524bdb
Wyatt Markham <wymark@colostate.edu>
2024-03-01 15:34:11 -0700

    Implemented CalculatorFactory

--- server/src/main/java/com/tco/misc/CalculatorFactory.java
+++ server/src/main/java/com/tco/misc/CalculatorFactory.java
@@ -5,7 +5,17 @@ public class CalculatorFactory {
     public CalculatorFactory() {}
 
     public GreatCircleDistance get(String formula) {
-        return null;
+        
+        String lower = formula.toLowerCase();
+
+        if (lower.equals("vincenty")) {
+            return new vincenty();
+        } else if (lower.equals("haversine")) {
+            return new haversine();
+        } else if (lower.equals("cosines")) {
+            return new cosines();
+        } else {
+            return null;
+        }
     }
-    
 }
