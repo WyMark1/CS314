1ef5128865fb21f8e83925bf955b2e900f2447b5
Austin Harris <hca1197@halibut.cs.colostate.edu>
2024-03-03 18:16:48 -0700

    added paramater checks to calculators

--- server/src/main/java/com/tco/misc/haversine.java
+++ server/src/main/java/com/tco/misc/haversine.java
@@ -6,6 +6,15 @@ public class haversine implements GreatCircleDistance {
     public haversine() {}
 
     public Long between(GeographicCoordinate geoCord1, GeographicCoordinate geoCord2, double radius) {
+
+        if (radius <= 1.0) {
+            throw new IllegalArgumentException("Radius must be greater than or equal to 1");
+        }
+
+        if (geoCord1 == null || geoCord2 == null) {
+            throw new IllegalArgumentException("Geographic coordinates cannot be null.");
+        }
+
         Double phi1 = geoCord1.latRadians();
         Double phi2 = geoCord2.latRadians();
         Double deltaPhi = phi2 - phi1;
--- server/src/main/java/com/tco/misc/vincenty.java
+++ server/src/main/java/com/tco/misc/vincenty.java
@@ -5,6 +5,15 @@ public class vincenty implements GreatCircleDistance {
     public vincenty () {}
     
     public Long between(GeographicCoordinate geoCord1, GeographicCoordinate geoCord2, double radius){
+
+        if (radius <= 1.0) {
+            throw new IllegalArgumentException("Radius must be greater than or equal to 1");
+        }
+
+        if (geoCord1 == null || geoCord2 == null) {
+            throw new IllegalArgumentException("Geographic coordinates cannot be null.");
+        }
+        
         Double phi1 = geoCord1.latRadians();
         Double phi2 = geoCord2.latRadians();
         Double deltaLambda = geoCord2.lonRadians()-geoCord1.lonRadians();
