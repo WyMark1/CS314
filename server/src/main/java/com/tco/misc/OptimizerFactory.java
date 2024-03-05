6e4091b544ec852bf62e7d3b4eaf247fa6d0878a
mstencel9 <mstencel@colostate.edu>
2024-03-05 16:05:16 -0700

    Removed Wrong changes from OptFactory file

--- server/src/main/java/com/tco/misc/OptimizerFactory.java
+++ server/src/main/java/com/tco/misc/OptimizerFactory.java
@@ -4,14 +4,6 @@ public class OptimizerFactory {
     public OptimizerFactory(){}
 
     public TourOptimizer get(int N, Double response){
-        if (N == 0) {
-            return new NoOptimizer();
-        } else if (N == 2) {
-            return new TwoOptimizer();
-        } else if (N == 3) {
-            return new ThreeOptimizer();
-        } else {
-            return null;
-        }   
+        return null;
     }
 }
