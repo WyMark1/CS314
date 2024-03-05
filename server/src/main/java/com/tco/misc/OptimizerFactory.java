fd312625c26657357ba2145a485d151ff697b57f
mstencel9 <mstencel@colostate.edu>
2024-03-05 15:58:08 -0700

    Modifying distances request to fit with calculator factory

--- server/src/main/java/com/tco/misc/OptimizerFactory.java
+++ server/src/main/java/com/tco/misc/OptimizerFactory.java
@@ -4,6 +4,14 @@ public class OptimizerFactory {
     public OptimizerFactory(){}
 
     public TourOptimizer get(int N, Double response){
-        return null;
+        if (N == 0) {
+            return new NoOptimizer();
+        } else if (N == 2) {
+            return new TwoOptimizer();
+        } else if (N == 3) {
+            return new ThreeOptimizer();
+        } else {
+            return null;
+        }   
     }
 }
