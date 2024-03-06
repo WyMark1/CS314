271802a982984f3db201f6bfc07f617b4df3e7af
Josh1302 <josh1302@colostate.edu>
2024-03-05 21:23:29 -0700

    Added code for get in OptimizerFactory

--- server/src/main/java/com/tco/misc/OptimizerFactory.java
+++ server/src/main/java/com/tco/misc/OptimizerFactory.java
@@ -4,6 +4,19 @@ public class OptimizerFactory {
     public OptimizerFactory(){}
 
     public TourOptimizer get(int N, Double response){
-        return null;
+        if(response != null){
+        if(N <=1 || response < 0.25 ){
+            return new NoOptimizer();
+        }
+        else if(N==2 || response <= 0.6){
+            return new TwoOptimizer();
+        }
+        else if (N>=3 || response <=1.0){
+            return new ThreeOptimizer();
+        }
+        else{
+            return new NoOptimizer();
+        }
+     }
     }
 }
