a4a02730112c27b9186542f2e7792e4f35529580
Josh1302 <josh1302@colostate.edu>
2024-03-05 21:53:47 -0700

    updated test

--- server/src/main/java/com/tco/misc/OptimizerFactory.java
+++ server/src/main/java/com/tco/misc/OptimizerFactory.java
@@ -4,19 +4,17 @@ public class OptimizerFactory {
     public OptimizerFactory(){}
 
     public TourOptimizer get(int N, Double response){
-        if(response != null){
         if(N <=1 || response < 0.25 ){
             return new NoOptimizer();
         }
-        else if(N==2 || response <= 0.6){
+        else if(N ==2 || response <= 0.6){
             return new TwoOptimizer();
         }
-        else if (N>=3 || response <=1.0){
+        else if (N ==3 || response <=1.0){
             return new ThreeOptimizer();
         }
         else{
             return new NoOptimizer();
         }
-     }
     }
 }
