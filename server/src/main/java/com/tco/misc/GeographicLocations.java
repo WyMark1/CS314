7704853e60f5516d2df2d18c37305f0f0f1b8b2a
Wyatt Markham <wymark@colostate.edu>
2024-03-28 17:40:53 -0600

    Added near to GeoLoc

--- server/src/main/java/com/tco/misc/GeographicLocations.java
+++ server/src/main/java/com/tco/misc/GeographicLocations.java
@@ -1,9 +1,17 @@
 package com.tco.misc;
 
+import com.tco.requests.Places;
+import com.tco.requests.Place;
+
 public class GeographicLocations {
 
     public GeographicLocations() {
 
+    }
+
+    public Places near(Place place, int distance, double eathRadius, String formula, int limit) {
+        Places places = new Places();
+        return places;
     }
 
 }
--- server/src/test/java/com/tco/misc/TestGeographicLocations.java
+++ server/src/test/java/com/tco/misc/TestGeographicLocations.java
@@ -1,5 +1,7 @@
 package com.tco.misc;
 
+import com.tco.misc.GeographicLocations;
+import com.tco.requests.Places;
 import com.tco.requests.Place;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
@@ -7,5 +9,16 @@ import org.junit.jupiter.api.DisplayName;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestGeographicLocations {
-
+    @Test
+    @DisplayName("wymark: Testing base case")
+    public void TestBaseCase() {
+        GeographicLocations geoloc = new GeographicLocations();
+        Place place = new Place(0.0, 0.0); 
+        int distance = 0;
+        double eathRadius = 0.0;
+        String formula = null;
+        int limit = 0;
+        Places result = new Places();
+        assertEquals(result, geoloc.near(place,distance,eathRadius,formula,limit));
+    }
 }
