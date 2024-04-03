a560a107a0152582cfdce304f7ba4fe64d600755
Austin Harris <hca1197@topeka.cs.colostate.edu>
2024-04-02 20:39:06 -0600

    Added sendSQL.java and testSendSQL.java

---
+++ server/src/main/java/com/tco/misc/sendSQL.java
@@ -0,0 +1,23 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import com.tco.requests.*;
+import com.tco.misc.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class sendSQL {
+    
+    public static ResultSet performQuery(String sql) throws Exception {
+        return null;
+    }
+
+    public static Places places(String sql) throws Exception {
+        return null;
+    }
+
+    static class Credential {
+    }
+
+}
