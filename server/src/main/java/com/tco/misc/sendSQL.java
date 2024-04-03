293d5309aa81833b3d1da975f224c7bb856efcfe
brownbpcolostate <brownbp@colostate.edu>
2024-04-03 15:23:30 -0600

    move the found method to sendSQL

--- server/src/main/java/com/tco/misc/sendSQL.java
+++ server/src/main/java/com/tco/misc/sendSQL.java
@@ -9,15 +9,41 @@ import java.util.HashMap;
 
 public class sendSQL {
     
+    static class Place extends HashMap<String, String> {}
+
+    static class Places extends ArrayList<Place> {}
+
     public static ResultSet performQuery(String sql) throws Exception {
-        return null;
+        try (
+            Connection conn = DriverManager.getConnection(Credential.URL, Credential.USER, Credential.PASSWORD);
+            Statement query = conn.createStatement();
+        ) {
+            return query.executeQuery(sql);
+        } catch (Exception e) {
+            throw new Exception("Error performing query: " + e.getMessage());
+        }
     }
 
     public static Places places(String sql) throws Exception {
-        return null;
+        ResultSet results = performQuery(sql);
+        Places places = new Places();
+        String[] columns = {"id", "name", "municipality", "iso_region", "iso_country", "latitude", "longitude", "altitude", "type"}; // Adjust columns as necessary
+        int count = 0;
+        while (results.next()) {
+            Place place = new Place();
+            for (String column : columns) {
+                place.put(column, results.getString(column));
+            }
+            place.put("index", String.format("%d", ++count));
+            places.add(place);
+        }
+        return places;
     }
 
     static class Credential {
+        final static String USER = "your_user_here";
+        final static String PASSWORD = "your_password_here";
+        final static String URL = "jdbc:mariadb://your_database_url_here";
     }
 
 }
