3ea7289dc50af82c8bc44616a2a1d1d679a1ab34
brownbpcolostate <brownbp@colostate.edu>
2024-04-04 12:51:53 -0600

    move the found method to sendSQL and testSendSQL

--- server/src/main/java/com/tco/misc/sendSQL.java
+++ server/src/main/java/com/tco/misc/sendSQL.java
@@ -8,42 +8,22 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class sendSQL {
+    final static String USER = "cs314-db";
+    final static String PASSWORD = "eiK5liet1uej";
+    final static String URL = "jdbc:mariadb://localhost:41311/cs314";
+
+    public Integer found(){
+        return 0;
+    }
     
-    static class Place extends HashMap<String, String> {}
-
-    static class Places extends ArrayList<Place> {}
-
-    public static ResultSet performQuery(String sql) throws Exception {
-        try (
-            Connection conn = DriverManager.getConnection(Credential.URL, Credential.USER, Credential.PASSWORD);
-            Statement query = conn.createStatement();
-        ) {
-            return query.executeQuery(sql);
-        } catch (Exception e) {
-            throw new Exception("Error performing query: " + e.getMessage());
-        }
+    public sendSQL() {}
+    
+    public ResultSet performQuery(String sql) throws Exception {
+        return null;
     }
 
-    public static Places places(String sql) throws Exception {
-        ResultSet results = performQuery(sql);
-        Places places = new Places();
-        String[] columns = {"id", "name", "municipality", "iso_region", "iso_country", "latitude", "longitude", "altitude", "type"}; // Adjust columns as necessary
-        int count = 0;
-        while (results.next()) {
-            Place place = new Place();
-            for (String column : columns) {
-                place.put(column, results.getString(column));
-            }
-            place.put("index", String.format("%d", ++count));
-            places.add(place);
-        }
-        return places;
-    }
-
-    static class Credential {
-        final static String USER = "your_user_here";
-        final static String PASSWORD = "your_password_here";
-        final static String URL = "jdbc:mariadb://your_database_url_here";
+    public Places places(String sql) throws Exception {
+        return null;
     }
 
 }
