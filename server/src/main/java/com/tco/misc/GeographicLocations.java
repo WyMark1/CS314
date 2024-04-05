f54163f15aa7e1b91983d3519ab34e63efa75098
Joshua Underwood <119467345+Josh1302@users.noreply.github.com>
2024-04-05 00:03:30 -0600

    Update GeographicLocations.java

--- server/src/main/java/com/tco/misc/GeographicLocations.java
+++ server/src/main/java/com/tco/misc/GeographicLocations.java
@@ -17,7 +17,7 @@ public class GeographicLocations {
         String lon = place.get("longitude");
         String select = "SELECT world.id, world.name, world.municipality, region.name AS region, country.name AS country, world.latitude, world.longitude, world.altitude, world.type, ABS(world.latitude - "+lat+") AS lat_diff,  ABS(world.longitude - "+lon+") AS lon_diff ";
         String from = "FROM continent INNER JOIN country ON continent.id = country.continent INNER JOIN region ON country.id = region.iso_country INNER JOIN world ON region.id = world.iso_region ";
-        String where =  "WHERE world.latitude BETWEEN " +lat+ " - 1 AND "+lat+" + 1  AND world.longitude BETWEEN" +lon+ " - 1 AND "+lon+" +1 " ;
+        String where =  "WHERE world.latitude BETWEEN " +lat+ " - 1 AND "+lat+" + 1  AND world.longitude BETWEEN " +lon+ " - 1 AND "+lon+" +1 " ;
         String orderBy = "ORDER BY lat_diff + lon_diff LIMIT "+Integer.toString(limit) +";";
         String sql = select + from + where + orderBy;
         sendSQL s = new sendSQL();
