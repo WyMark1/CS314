722c8e883c0de0e48d1f093be08104f1293b3d39
mstencel9 <mstencel@colostate.edu>
2024-04-08 15:22:12 -0600

    Implementing the Distances Method

--- server/src/main/java/com/tco/misc/GeographicLocations.java
+++ server/src/main/java/com/tco/misc/GeographicLocations.java
@@ -13,11 +13,11 @@ public class GeographicLocations {
 
     }
 
-    public Places near(Place place, int distance, double eathRadius, String formula, int limit) throws BadRequestException {
+    public Places near(Place place, int distance, double earthRadius, String formula, int limit) throws BadRequestException {
         String lat = place.get("latitude");
         String lon = place.get("longitude");
-        double latDelta = (distance / eathRadius) * (180 / Math.PI);
-        double lonDelta = (distance / eathRadius) * (180 / Math.PI) / Math.cos(place.latRadians());
+        double latDelta = (distance / earthRadius) * (180 / Math.PI);
+        double lonDelta = (distance / earthRadius) * (180 / Math.PI) / Math.cos(place.latRadians());
         String select = "SELECT world.id, world.name, world.municipality, region.name AS region, country.name AS country, world.latitude, world.longitude, world.altitude, world.type, ABS(world.latitude - "+lat+") AS lat_diff,  ABS(world.longitude - "+lon+") AS lon_diff ";
         String from = "FROM continent INNER JOIN country ON continent.id = country.continent INNER JOIN region ON country.id = region.iso_country INNER JOIN world ON region.id = world.iso_region ";
         String where =  "WHERE world.latitude BETWEEN " +lat+ " - "+latDelta+" AND "+lat+" + "+latDelta+"  AND world.longitude BETWEEN " +lon+ " - "+ lonDelta +" AND "+lon+" + " + lonDelta;
@@ -27,8 +27,17 @@ public class GeographicLocations {
         return s.places(sql);
     }
 
-    public Distances distances(Place place, Places places) {
+    public Distances distances(Place place, Places places, double earthRadius, String formula) {
         Distances distances = new Distances();
+        CalculatorFactory calcFac = new CalculatorFactory();
+        GreatCircleDistance formulaType = calcFac.get(formula);
+
+        for (Place p : places) {
+            if (!place.equals(p)) {
+                distances.add(formulaType.between(place, p, earthRadius));
+            }
+        }
+        
         return distances;
     }
 
