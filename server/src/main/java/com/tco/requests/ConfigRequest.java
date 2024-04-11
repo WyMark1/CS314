a193be56feee855af3d4ed9df9f5498004695542
Wyatt Markham <wymark@colostate.edu>
2024-04-10 21:20:18 -0600

    Fixed the issue but need to add more countries

--- server/src/main/java/com/tco/requests/ConfigRequest.java
+++ server/src/main/java/com/tco/requests/ConfigRequest.java
@@ -2,7 +2,7 @@ package com.tco.requests;
 
 import java.util.List;
 import java.util.ArrayList;
-
+import java.util.Arrays;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,12 +13,16 @@ public class ConfigRequest extends Request {
     private String serverName;
     private List<String> features;
     private List<String> formulae;
+    private List<String> type;
+    private List<String> where;
 
     @Override
     public void buildResponse() {
         serverName = "t11 BoneYard";
         features = new ArrayList<>();
         formulae = new ArrayList<>();
+        type = Arrays.asList("airport","balloonport","heliport","other");
+        where = Arrays.asList("United States");
         features.add("config");
         features.add("distances");
         features.add("tour");
