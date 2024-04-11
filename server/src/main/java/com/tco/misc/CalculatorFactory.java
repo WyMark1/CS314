5375fc83df7eaa8fa95a23ee98966a5f85f54e3f
brownbpcolostate <brownbp@colostate.edu>
2024-04-10 22:40:15 -0600

    Removed Code Smells in OptimizerFactory

--- server/src/main/java/com/tco/misc/CalculatorFactory.java
+++ server/src/main/java/com/tco/misc/CalculatorFactory.java
@@ -1,27 +1,33 @@
 package com.tco.misc;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
 public class CalculatorFactory {
+
+    private static final Map<String, Supplier<GreatCircleDistance>> formulaMap = new HashMap<>();
+
+    static {
+        formulaMap.put("vincenty", vincenty::new);
+        formulaMap.put("haversine", haversine::new);
+        formulaMap.put("cosines", cosines::new);
+    }
 
     public CalculatorFactory() {}
 
     public static GreatCircleDistance get(String formula) throws IllegalArgumentException {
-        
-        if (formula != null) {
-            String lower = formula.toLowerCase();
-
-            if (lower.equals("vincenty")) {
-                return new vincenty();
-            } else if (lower.equals("haversine")) {
-                return new haversine();
-            } else if (lower.equals("cosines")) {
-                return new cosines();
-            } else {
-                throw new IllegalArgumentException("Unsupported formula: " + formula);
-            }
-        } 
-        else {
+        if (formula == null) {
             return new vincenty();
         }
-
+        
+        String lower = formula.toLowerCase();
+        Supplier<GreatCircleDistance> supplier = formulaMap.get(lower);
+        
+        if (supplier != null) {
+            return supplier.get();
+        } else {
+            throw new IllegalArgumentException("Unsupported formula: " + formula);
+        }
     }
 }
